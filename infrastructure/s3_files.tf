74c97d6cd016ef175a68c392f148037ee78b33a8
Adriano Fonseca <30940643+adrianofonseca1000@users.noreply.github.com>
2022-09-07 15:00:58 -0300

    backend tf AWS s3

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -5,3 +5,35 @@ resource "aws_s3_object" "codigo_spark" {
   source = "../job_spark_emr.py"
   etag   = filemd5("../job_spark_emr.py")
 }
+
+resource "aws_s3_bucket_object" "script_athena" {
+    bucket = aws_s3_bucket.datalake.id
+    key    = "athena-results/"
+    acl    = "private"
+    source = "../.gitkeep"
+    etag   = filemd5("../.gitkeep")
+}
+
+resource "aws_s3_bucket_object" "zona_consumo" {
+    bucket = aws_s3_bucket.datalake.id
+    key    = "consumer-zone/"
+    acl    = "private"
+    source = "../.gitkeep"
+    etag   = filemd5("../.gitkeep")
+}
+
+resource "aws_s3_bucket_object" "script_glue" {
+    bucket = aws_s3_bucket.datalake.id
+    key    = "glue-scripts/"
+    acl    = "private"
+    source = "../job_spark_glue.py"
+    etag   = filemd5("../job_spark_glue.py")
+}
+
+resource "aws_s3_bucket_object" "dado_nao_tratado" {
+    bucket = aws_s3_bucket.datalake.id
+    key    = "raw-data/"
+    acl    = "private"
+    source = "../.gitkeep"
+    etag   = filemd5("../.gitkeep")
+}
